33f3b4994c02d4039b24ac1f548e3239b472b747
Erich Buri <buri@schaltstelle.ch>
2021-03-11 14:41:20 +0100

    send file

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 
 const app = express()
 const port = process.env.port || 8080;
-const imgDirPath = process.env.FilePath || path.join(__dirname,  'assets');
+const imgDirPath = process.env.FilePath || path.join(__dirname, 'assets');
+
+app.set('trust proxy', 1) // Trust env-settings from load-balancer
 
 app.get('/', (req, res) => {
     fs.readdir(imgDirPath, (err, files) => {
@@ -15,6 +17,15 @@ app.get('/', (req, res) => {
 
         res.status(200).json(files)
     });
+})
+
+// unsafe file delivery...
+app.get('/file/:id', (req, res) =>
+    res.sendFile(path.join(imgDirPath, req.params.id))
+)
+
+app.get('/_env', (req, res) => {
+    res.status(200).json(process.env);
 })
 
 app.listen(port, () => {
